7d4a07e946aec72f196602593f06274e1885d41c
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-04-27 21:30:43 +0200

    added register page

--- bethins/app.py
+++ bethins/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from bethins.views.index import bp as index_bp
 from bethins.views.profile import bp as profile_bp
+from bethins.views.register import bp as register_bp
 
 from bethins.api.site import is_loggedin
 
@@ -8,5 +9,6 @@ app = Flask(__name__)
 
 app.register_blueprint(index_bp)
 app.register_blueprint(profile_bp)
+app.register_blueprint(register_bp)
 
 app.jinja_env.globals.update(is_loggedin=is_loggedin)
